b77e33a37ebc28fb8940731f7b9600e60b929574
GabrielaJ01 <91990192+GabrielaJ01@users.noreply.github.com>
2021-11-08 17:25:43 -0500

    Add files via upload

---
+++ Informática1/tareavariables.js
@@ -0,0 +1,11 @@
+var numero1 , numero2 , suma , resta , multiplicación , división
+numero1 = prompt("Ingrese el primer número");
+numero2 = prompt ("Ingrese el segundo número");
+suma = (parseInt(numero1)+parseInt(numero2));
+resta = (parseInt(numero1)-parseInt(numero2));
+multiplicación = (parseInt(numero1)*parseInt(numero2));
+división = (parseInt(numero1)/parseInt(numero2));
+console.log (`La suma es: ${suma}`)
+console.log (`La resta es: ${resta}`)
+console.log (`La multiplicación es: ${multiplicación}`)
+console.log (`La división es: ${división}`)
